54b55fcb423b58cc70b135cdbd844a8737fc9e98
firerzaadit-prog <firerzaadit@gmail.com>
2026-03-28 21:36:03 +0700

    BARU 28032026

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,11 @@
-// supabaseClient.js - Untuk Admin Panel
+// supabaseClient.js - Untuk Client / Frontend
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://tsgldkyuktqpsbeuevsn.supabase.co';
-// Gunakan SERVICE ROLE KEY untuk admin access
-const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZ2xka3l1a3RxcHNiZXVldnNuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzY5MTE5OSwiZXhwIjoyMDc5MjY3MTk5fQ.0ovVI04BphGbSCSZE6EhHI6_rtZCPSZReiaM9dKYcIA';
 
-const supabase = createClient(supabaseUrl, supabaseServiceKey);
+// Gunakan ANON KEY (Public) untuk akses dari browser/frontend
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsIn... (dan seterusnya panjang sekali) ...';
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export { supabase };
